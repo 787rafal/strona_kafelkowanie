c44b0827feccc6eaaf2c1e90edb1f1c56e896d8e
Rafal <rafalbarton787@gmail.com>
2022-11-04 10:37:49 +0100

    Zajecia 4.11

--- scripts/menu.js
+++ scripts/menu.js
@@ -4,12 +4,14 @@ function menu(){
     var men = document.querySelector("#ham_option");
     if(bool == false)
     {
-        men.style.display = "block";
+        //men.style.display = "block";
+        men.classList.add('js_ham');
         bool = true;
     }
     else if (bool == true)
     {
-        men.style.display = "none";
+        //men.style.display = "none";
+        men.classList.remove('js_ham');
         bool = false;
     }
     console.log(men);
