b92def0ba41cd9878cad1718ac11eab346f04b29
Rafal <rafalbarton787@gmail.com>
2022-12-02 10:33:37 +0100

    Lekcja 02.12

--- html/js/remove.js
+++
@@ -1,8 +0,0 @@
-// Create Element.remove() function if not exist
-if (!('remove' in Element.prototype)) {
-    Element.prototype.remove = function() {
-        if (this.parentNode) {
-            this.parentNode.removeChild(this);
-        }
-    };
-}
